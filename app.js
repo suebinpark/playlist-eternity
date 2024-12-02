9b3ed7b6d0979e1a88b94010fa007d2374d23905
Suebin Park <90715726+suebinpark@users.noreply.github.com>
2024-12-03 04:35:43 +0900

    cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const path = require("path");
 const cors = require("cors");
+const base_url =
+  "https://port-0-playlist-eternity-m47c1rlvf34a3653.sel4.cloudtype.app/";
 
 dotenv.config();
 const entriesRouter = require("./routes/comments");
@@ -10,7 +12,13 @@ const entriesRouter = require("./routes/comments");
 const app = express();
 app.set("port", process.env.PORT || 8080);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: base_url, // 클라이언트 배포 URL
+    methods: ["GET", "POST", "DELETE"], // 허용할 HTTP 메서드
+    credentials: true, // 인증 정보 포함 여부
+  })
+);
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public_2024")));
 app.use(express.json());
